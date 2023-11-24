21c9c81c95ab4f87d4830b67027627ff974fbaec
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-11-24 15:58:19 +0100

    added initial files

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP ToDo List JSON</title>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+
+<body>
+
+    <div id="app">
+        <section>
+            <div class="container">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="input-group">
+
+                        </div>
+                    </div>
+                    <ul>
+                        <li>
+
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+
+    <script src="./js/main.js"></script>
+</body>
+
+</html>
